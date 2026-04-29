f6433def402c2fdc05a5b1e7cd1fe2dced971631
Paul Frick <70173526+PaulanerAlex@users.noreply.github.com>
2026-04-29 14:44:01 +0200

    Enhance start_server.sh with informative echo statements
    Added messages for starting screen session and server exit instructions.

--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo "Starting screen session..."
+echo | <screen>
+
+
+
 echo "Syncing uv environment..."
 uv sync
 
-echo "Starting Nextcloud YOLO Training Server..."
+echo "Starting Nextcloud YOLO Training Server. To exit, type strg + A and then D ..."
 uv run src/yolo_training_tools/server.py
